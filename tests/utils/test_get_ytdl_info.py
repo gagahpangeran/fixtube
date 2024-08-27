8c187d68f55d135f378edeea671faba739cbd7d4
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-27 12:43:43 +0700

    mock ytdl extract_info method

--- tests/utils/test_get_ytdl_info.py
+++ tests/utils/test_get_ytdl_info.py
@@ -1,7 +1,14 @@
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import YoutubeDLError
 from fixtube.utils import get_ytdl_info
 
 
-def test_get_ytdl_info_valid_id():
+def test_get_ytdl_info_valid_id(monkeypatch, video_info):
+    def mock_extract_info(*args, **kwargs):
+        return video_info
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
     video_id = "dQw4w9WgXcQ"
     info = get_ytdl_info(video_id)
 
@@ -12,15 +19,13 @@ def test_get_ytdl_info_valid_id():
     assert "formats" in info
 
 
-def test_get_ytdl_info_not_found_id():
+def test_get_ytdl_info_not_found_id(monkeypatch, video_info):
+    def mock_extract_info(*args, **kwargs):
+        raise YoutubeDLError("video not found")
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
     video_id = "randomvideo"
-    info = get_ytdl_info(video_id)
-
-    assert info is None
-
-
-def test_get_ytdl_info_invalid_id():
-    video_id = "abc"
     info = get_ytdl_info(video_id)
 
     assert info is None
