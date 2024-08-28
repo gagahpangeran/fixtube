031ebb8bd7872624b4f2334cfb9b47c20682c2ff
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-28 19:55:54 +0700

    update routes watch tests

--- tests/routes/test_watch.py
+++ tests/routes/test_watch.py
@@ -1,16 +1,32 @@
-from flask.testing import FlaskClient
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import YoutubeDLError
 
 
-def test_watch_id(client: FlaskClient):
-    response = client.get("/watch?v=abcd")
-    assert response.data == b"abcd"
+def test_watch_id(client, monkeypatch, video_info):
+    def mock_extract_info(*args, **kwargs):
+        return video_info
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
+    response = client.get("/dQw4w9WgXcQ")
+    assert response.status_code == 200
 
 
-def test_watch_id_empty(client: FlaskClient):
+def test_watch_empty_id(client):
     response = client.get("/watch?v=")
     assert response.status_code == 404
 
 
-def test_watch_no_id(client: FlaskClient):
+def test_watch_no_id(client):
     response = client.get("/watch")
+    assert response.status_code == 404
+
+
+def test_watch_invalid_id(client, monkeypatch):
+    def mock_extract_info(*args, **kwargs):
+        raise YoutubeDLError("video not found")
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
+    response = client.get("/randomvideo")
     assert response.status_code == 404
