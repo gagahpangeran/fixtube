1d13b84d1bbeb067839c8ba83560586450876e4d
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-28 19:55:38 +0700

    update routes live tests

--- tests/routes/test_live.py
+++ tests/routes/test_live.py
@@ -1,16 +1,32 @@
-from flask.testing import FlaskClient
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import YoutubeDLError
 
 
-def test_live_id(client: FlaskClient):
-    response = client.get("/live/abcd")
-    assert response.data == b"abcd"
+def test_live_valid_id(client, monkeypatch, video_info):
+    def mock_extract_info(*args, **kwargs):
+        return video_info
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
+    response = client.get("/dQw4w9WgXcQ")
+    assert response.status_code == 200
 
 
-def test_live_id_empty(client: FlaskClient):
+def test_live_empty_id(client):
     response = client.get("/live/  ")
     assert response.status_code == 404
 
 
-def test_live_no_id(client: FlaskClient):
+def test_live_no_id(client):
     response = client.get("/live/")
+    assert response.status_code == 404
+
+
+def test_live_invalid_id(client, monkeypatch):
+    def mock_extract_info(*args, **kwargs):
+        raise YoutubeDLError("video not found")
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
+    response = client.get("/randomvideo")
     assert response.status_code == 404
