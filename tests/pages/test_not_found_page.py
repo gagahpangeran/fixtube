43d94ed5d54a163b8562ddae842c3ec31891ff61
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-29 00:00:12 +0700

    add test for not found page

---
+++ tests/pages/test_not_found_page.py
@@ -0,0 +1,14 @@
+def test_index_page(client):
+    response = client.get("/ok/")
+    data = response.data.decode()
+
+    assert response.status_code == 404
+    assert "<title>Video not found</title>" in data
+    assert "<meta name=\"description\" content=\"404 not found.\" />" in data
+    assert "<link rel=\"canonical\" href=\"https://www.youtube.com\" />" in data
+    assert "<meta http-equiv=\"refresh\" content=\"0; url=https://www.youtube.com\" />"
+    assert "<meta property=\"og:title\" content=\"Video not found\" />" in data
+    assert "<meta property=\"og:description\" content=\"404 not found.\" />" in data
+    assert "<meta property=\"og:site_name\" content=\"FixTube\" />" in data
+    assert "<meta property=\"og:type\" content=\"website\" />" in data
+    assert "<meta property=\"og:video\"" not in data
