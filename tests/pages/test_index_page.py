28ee08a71b0910230380cfff0794b4e5af4d9844
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-28 23:48:02 +0700

    add test for index page

---
+++ tests/pages/test_index_page.py
@@ -0,0 +1,13 @@
+def test_index_page(client):
+    response = client.get("/")
+    data = response.data.decode()
+
+    assert "<title>FixTube</title>" in data
+    assert "<meta name=\"description\" content=\"Fix your youtube embed.\" />" in data
+    assert "<link rel=\"canonical\" href=\"https://www.youtube.com\" />" in data
+    assert "<meta http-equiv=\"refresh\" content=\"0; url=https://www.youtube.com\" />"
+    assert "<meta property=\"og:title\" content=\"FixTube\" />" in data
+    assert "<meta property=\"og:description\" content=\"Fix your youtube embed.\" />" in data
+    assert "<meta property=\"og:site_name\" content=\"FixTube\" />" in data
+    assert "<meta property=\"og:type\" content=\"website\" />" in data
+    assert "<meta property=\"og:video\"" not in data
