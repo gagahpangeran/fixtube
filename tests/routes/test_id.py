190e5222a5482604bb3e049b9d99e83c946d789e
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-28 19:55:30 +0700

    update routes id tests

--- tests/routes/test_id.py
+++ tests/routes/test_id.py
@@ -1,11 +1,27 @@
-from flask.testing import FlaskClient
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import YoutubeDLError
 
 
-def test_id(client: FlaskClient):
-    response = client.get("/abcd")
-    assert response.data == b"abcd"
+def test_id_valid_id(client, monkeypatch, video_info):
+    def mock_extract_info(*args, **kwargs):
+        return video_info
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
+    response = client.get("/dQw4w9WgXcQ")
+    assert response.status_code == 200
 
 
-def test_id_empty(client: FlaskClient):
+def test_id_empty_id(client):
     response = client.get("/  ")
+    assert response.status_code == 404
+
+
+def test_id_invalid_id(client, monkeypatch):
+    def mock_extract_info(*args, **kwargs):
+        raise YoutubeDLError("video not found")
+
+    monkeypatch.setattr(YoutubeDL, "extract_info", mock_extract_info)
+
+    response = client.get("/randomvideo")
     assert response.status_code == 404
