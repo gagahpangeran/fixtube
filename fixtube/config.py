d610fc309882cfd9b0a0259c4bb0126533589e8d
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-08-31 15:05:15 +0700

    add HOME_PAGE and ISSUE_PAGE config

--- fixtube/config.py
+++ fixtube/config.py
@@ -2,3 +2,6 @@ from os import environ
 
 CACHE_TYPE = environ.get("CACHE_TYPE", "SimpleCache")
 CACHE_DEFAULT_TIMEOUT = int(environ.get("CACHE_DEFAULT_TIMEOUT", "300"))
+
+HOME_PAGE = environ.get("HOME_PAGE", "")
+ISSUE_PAGE = environ.get("ISSUE_PAGE", "")
