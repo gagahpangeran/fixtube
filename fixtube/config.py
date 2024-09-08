9f5cc828d228aa30165b175f95618bd1cab4c1df
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-09-09 02:00:01 +0700

    update app version to 2024.09.09

--- fixtube/config.py
+++ fixtube/config.py
@@ -1,6 +1,6 @@
 from os import environ
 
-APP_VERSION = "2024.09.07"
+APP_VERSION = "2024.09.09"
 
 CACHE_TYPE = environ.get("CACHE_TYPE", "SimpleCache")
 CACHE_DEFAULT_TIMEOUT = int(environ.get("CACHE_DEFAULT_TIMEOUT", "300"))
