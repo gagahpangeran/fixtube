9c95383ed85ddc3539bc804f868baee99b887bc3
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-09-08 15:19:11 +0700

    add redis config and env

--- fixtube/config.py
+++ fixtube/config.py
@@ -5,5 +5,9 @@ APP_VERSION = "2024.09.07"
 CACHE_TYPE = environ.get("CACHE_TYPE", "SimpleCache")
 CACHE_DEFAULT_TIMEOUT = int(environ.get("CACHE_DEFAULT_TIMEOUT", "300"))
 
+CACHE_REDIS_HOST = environ.get("CACHE_REDIS_HOST")
+CACHE_REDIS_PORT = environ.get("CACHE_REDIS_PORT")
+CACHE_REDIS_DB = environ.get("CACHE_REDIS_DB")
+
 HOME_PAGE = environ.get("HOME_PAGE", "")
 ISSUE_PAGE = environ.get("ISSUE_PAGE", "")
